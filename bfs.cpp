642ae833f14d1584d4daf9ab12ecd91a35672bd3
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2019-06-10 00:51:44 +0530

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,17 @@
+queue<ll> q;
+q.push(1);
+rank[1]=0;
+while(!q.empty())
+{
+    ll x=q.front();
+    q.pop();
+    visited[x]=1;
+    for(ll i=0;i<adj[x].size();i++)
+    {
+        if(visited[adj[x][i]])
+            continue;
+        rank[adj[x][i]]=rank[x]+1;
+        q.push(adj[x][i]);
+        visited[adj[x][i]]=1;
+    }
+}
