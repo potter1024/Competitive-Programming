45fd4361f2f222fdb82558413f5cef366d5948f4
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2020-03-27 15:28:13 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,38 +1,9 @@
-//#include <bits/stdc++.h>
-#include <iostream>
-#include <climits>
-#include <vector>
-#include <cstdlib>
-#include <iomanip>
-#include <algorithm>
-#include <fstream>
-#include <cmath>
-#include <math.h>
-#include <set>
-#include <time.h>
-#include <map>
-#include <fstream>
-#include <cstdio>
-#include <vector>
-#include <cstring>
-#include <string>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <complex>
-#include <numeric>
-#include <unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
 #define sz(a)               int((a).size())
 #define ll                  long long
 #define pb                  push_back
 #define ld                  long double
-#define pi                  pair<ll,ll>
 #define mp                  make_pair
 #define vi                  vector<ll>
 #define vii                  vector<pi>
@@ -44,16 +15,13 @@ using namespace std;
 #define rep(i,n)            for(ll i=0;i<n;i++)
 #define cpresent(c,x)       (find(all(c),x) != (c).end())
 #define input(a,n)          for(long long i=0;i<n;i++)cin>>a[i]
-#define output(a,n)         for(long long i=0;i<n;i++)cout<<a[i]<<" "
-#define inputt(a,n,m)       for(ll i=0;i<n;i++)for(ll j=0;j<m;j++)cin>>a[i][j]
-#define outputt(a,n,m)      for(ll i=0;i<n;i++){for(ll j=0;j<m;j++)cout<<a[i][j]<<" ";cout<<endl;}
+#define output(a)           for(long long i=0;i<a.size();i++)cout<<a[i]<<" "
 #define reset(a,n)          memset(a,n,sizeof(a))   // n can only be 0 or -1
-#define vl                  LLONG_MIN
-#define vm                  LLONG_MAX
 #define ep                  1e-10
 #define seed                13331
-#define inf                 1e10
-#define N                   1000000
+#define pi                  pair<ll,ll>
+#define inf                 10e9
+#define N                   2000000
 #define minp                vector<ll>, greater<ll>
 #define M                   1000000007
 #define MM                  1000000009
@@ -63,38 +31,64 @@ using namespace std;
 #define time     cout<<(double(clock()-startTime )/(double)CLOCKS_PER_SEC)*1000<<" ms"<<endl;
 #define debug(k) cout<<"\t-> "<<#k<<" = "<<k<<endl;
 
-/************************************Debugging Steps*****************************************/
+/************************************Debugging Steps****************************************/
 
 // If aplying ceil,floor,pow,etc convert it into integer (might make you eat shit if you don't
 // Whenever using stacks,queues,etc always check that their top / front / back elements are not accessed when they are empty, thus causing runtime error
 // Think twice before using greedy
 // Remember losing 1 or 2 minutes is better than a penalty of 10 minutes
 // Use log2 instead of log()/log(2) to avoid shitty errors
-// If MLE occurs, try declaring the large array in int instead of long long
+// If declaring a large array, define it globally
 // Always initialise anything while declaring, which will not be taken as input
 // In case of a problem having decimal calculations, try declaring everything in double rather than typecasting in each step
 // always declare the array with n+5 elements to avoid unexpected errors
 // Every problem cannot be solved by DFS easily, think of BFS too
-
-bool sortbysec(const pair<int,int> &a,const pair<int,int> &b)
-{
-    return (a.second < b.second);
+// Never use accumulate
+bool sortbysec(const pair<ll,pair<ll,ll> > &a,const pair<ll,pair<ll,ll> > &b){
+    return (a.ss.ff < b.ss.ff);
 }
-ll gcd(ll a,ll b)
-{
+bool sortbyth(const pair<ll,pair<ll,ll> > &a,const pair<ll,pair<ll,ll> > &b){
+    return (a.ss.ss < b.ss.ss);
+}
+ll gcd(ll a,ll b){
     return b?gcd(b, a % b):a;
 }
-ll min(ll a,ll b)
-{
+ll lcm(ll a,ll b){
+    return (a*b)/gcd(a,b);
+}
+ll min(ll a,ll b){
     return a<b?a:b;
 }
-ll max(ll a,ll b)
-{
+ll max(ll a,ll b){
     return a>b?a:b;
 }
+ll max3(ll a,ll b,ll c){
+    return max(max(a,b),c);
+}
+ll max4(ll a,ll b,ll c,ll d){
+    return max(max(a,b),max(c,d));
+}
+ll min3(ll a,ll b,ll c){
+    return min(min(a,b),c);
+}
+ll min4(ll a,ll b,ll c,ll d){
+    return min(min(a,b),min(c,d));
+}
+ll po(ll x,ll n,ll mod){
+    if(n==0){
+        return 1;
+    }
+    ll m=po(x,n/2,mod);
+    m*=m;
+    m%=mod;
+    if(n%2){
+        m*=x;
+        m%=mod;
+    }
+    return m;
+}
+int main(){
 
-int32_t main()
-{
     she_taught_me_to_code
 
     // freopen("input.txt","r",stdin);
