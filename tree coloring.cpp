dfb62603155916dc7169f26a58577c64e5e908c8
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2020-03-31 00:42:41 +0530

    Create tree coloring.cpp

---
+++ tree coloring.cpp
@@ -0,0 +1,270 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(a)               int((a).size())
+#define ll                  long long
+#define pb                  push_back
+#define ld                  long double
+#define mp                  make_pair
+#define vi                  vector<ll>
+#define vii                  vector<pi>
+#define endl                "\n"
+#define ff                  first
+#define ss                  second
+#define all(c)              (c).begin(),(c).end()
+#define allr(c)             (c).rbegin(),(c).rend()
+#define rep(i,n)            for(ll i=0;i<n;i++)
+#define cpresent(c,x)       (find(all(c),x) != (c).end())
+#define input(a,n)          for(long long i=0;i<n;i++)cin>>a[i]
+#define output(a)           for(long long i=0;i<a.size();i++)cout<<a[i]<<" "
+#define reset(a,n)          memset(a,n,sizeof(a))   // n can only be 0 or -1
+#define ep                  1e-10
+#define seed                13331
+#define pi                  pair<ll,ll>
+#define inf                 10e9
+#define N                   1000000
+#define minp                vector<ll>, greater<ll>
+#define M                   1000000007
+#define MM                  1000000009
+#define pie                 3.1415926535897932384626433832795
+#define eu                  2.718281828459045
+#define                     she_taught_me_to_code cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);cin.tie(NULL);    clock_t startTime=clock();
+#define time     cout<<(double(clock()-startTime )/(double)CLOCKS_PER_SEC)*1000<<" ms"<<endl;
+#define debug(k) cout<<"\t-> "<<#k<<" = "<<k<<endl;
+
+/************************************Debugging Steps****************************************/
+
+// If aplying ceil,floor,pow,etc convert it into integer (might make you eat shit if you don't
+// Whenever using stacks,queues,etc always check that their top / front / back elements are not accessed when they are empty, thus causing runtime error
+// Think twice before using greedy
+// Remember losing 1 or 2 minutes is better than a penalty of 10 minutes
+// Use log2 instead of log()/log(2) to avoid shitty errors
+// If declaring a large array, define it globally
+// Always initialise anything while declaring, which will not be taken as input
+// In case of a problem having decimal calculations, try declaring everything in double rather than typecasting in each step
+// always declare the array with n+5 elements to avoid unexpected errors
+// Every problem cannot be solved by DFS easily, think of BFS too
+// Never use accumulate
+bool sortbysec(const pair<ll,pair<ll,ll> > &a,const pair<ll,pair<ll,ll> > &b){
+    return (a.ss.ff < b.ss.ff);
+}
+bool sortbyth(const pair<ll,pair<ll,ll> > &a,const pair<ll,pair<ll,ll> > &b){
+    return (a.ss.ss < b.ss.ss);
+}
+ll gcd(ll a,ll b){
+    return b?gcd(b, a % b):a;
+}
+ll lcm(ll a,ll b){
+    return (a*b)/gcd(a,b);
+}
+ll min(ll a,ll b){
+    return a<b?a:b;
+}
+ll max(ll a,ll b){
+    return a>b?a:b;
+}
+ll max3(ll a,ll b,ll c){
+    return max(max(a,b),c);
+}
+ll max4(ll a,ll b,ll c,ll d){
+    return max(max(a,b),max(c,d));
+}
+ll min3(ll a,ll b,ll c){
+    return min(min(a,b),c);
+}
+ll min4(ll a,ll b,ll c,ll d){
+    return min(min(a,b),min(c,d));
+}
+ll po(ll x,ll n,ll mod){
+    if(n==0){
+        return 1;
+    }
+    ll m=po(x,n/2,mod);
+    m*=m;
+    m%=mod;
+    if(n%2){
+        m*=x;
+        m%=mod;
+    }
+    return m;
+}
+const int INF = 1000 * 1000 * 1000;
+
+typedef vector<vector<int>> graph;
+
+vector<int> dfs_list;
+vector<int> edges_list;
+vector<int> h;
+
+void dfs(int v, const graph& g, const graph& edge_ids, int cur_h = 1) {
+    h[v] = cur_h;
+    dfs_list.push_back(v);
+    for (size_t i = 0; i < g[v].size(); ++i) {
+        if (h[g[v][i]] == -1) {
+            edges_list.push_back(edge_ids[v][i]);
+            dfs(g[v][i], g, edge_ids, cur_h + 1);
+            edges_list.push_back(edge_ids[v][i]);
+            dfs_list.push_back(v);
+        }
+    }
+}
+
+vector<int> lca_tree;
+vector<int> first;
+
+void lca_tree_build(int i, int l, int r) {
+    if (l == r) {
+        lca_tree[i] = dfs_list[l];
+    } else {
+        int m = (l + r) >> 1;
+        lca_tree_build(i + i, l, m);
+        lca_tree_build(i + i + 1, m + 1, r);
+        int lt = lca_tree[i + i], rt = lca_tree[i + i + 1];
+        lca_tree[i] = h[lt] < h[rt] ? lt : rt;
+    }
+}
+
+void lca_prepare(int n) {
+    lca_tree.assign(dfs_list.size() * 8, -1);
+    lca_tree_build(1, 0, (int)dfs_list.size() - 1);
+
+    first.assign(n, -1);
+    for (int i = 0; i < (int)dfs_list.size(); ++i) {
+        int v = dfs_list[i];
+        if (first[v] == -1)
+            first[v] = i;
+    }
+}
+
+int lca_tree_query(int i, int tl, int tr, int l, int r) {
+    if (tl == l && tr == r)
+        return lca_tree[i];
+    int m = (tl + tr) >> 1;
+    if (r <= m)
+        return lca_tree_query(i + i, tl, m, l, r);
+    if (l > m)
+        return lca_tree_query(i + i + 1, m + 1, tr, l, r);
+    int lt = lca_tree_query(i + i, tl, m, l, m);
+    int rt = lca_tree_query(i + i + 1, m + 1, tr, m + 1, r);
+    return h[lt] < h[rt] ? lt : rt;
+}
+
+int lca(int a, int b) {
+    if (first[a] > first[b])
+        swap(a, b);
+    return lca_tree_query(1, 0, (int)dfs_list.size() - 1, first[a], first[b]);
+}
+
+vector<int> first1, first2;
+vector<char> edge_used;
+vector<int> tree1, tree2;
+
+void query_prepare(int n) {
+    first1.resize(n - 1, -1);
+    first2.resize(n - 1, -1);
+    for (int i = 0; i < (int)edges_list.size(); ++i) {
+        int j = edges_list[i];
+        if (first1[j] == -1)
+            first1[j] = i;
+        else
+            first2[j] = i;
+    }
+
+    edge_used.resize(n - 1);
+    tree1.resize(edges_list.size() * 8);
+    tree2.resize(edges_list.size() * 8);
+}
+
+void sum_tree_update(vector<int>& tree, int i, int l, int r, int j, int delta) {
+    tree[i] += delta;
+    if (l < r) {
+        int m = (l + r) >> 1;
+        if (j <= m)
+            sum_tree_update(tree, i + i, l, m, j, delta);
+        else
+            sum_tree_update(tree, i + i + 1, m + 1, r, j, delta);
+    }
+}
+
+int sum_tree_query(const vector<int>& tree, int i, int tl, int tr, int l, int r) {
+    if (l > r || tl > tr)
+        return 0;
+    if (tl == l && tr == r)
+        return tree[i];
+    int m = (tl + tr) >> 1;
+    if (r <= m)
+        return sum_tree_query(tree, i + i, tl, m, l, r);
+    if (l > m)
+        return sum_tree_query(tree, i + i + 1, m + 1, tr, l, r);
+    return sum_tree_query(tree, i + i, tl, m, l, m) +
+           sum_tree_query(tree, i + i + 1, m + 1, tr, m + 1, r);
+}
+
+int query(int v1, int v2) {
+    return sum_tree_query(tree1, 1, 0, (int)edges_list.size() - 1, first[v1], first[v2] - 1) -
+           sum_tree_query(tree2, 1, 0, (int)edges_list.size() - 1, first[v1], first[v2] - 1);
+}
+
+int main() {
+    // reading the graph
+    int n;
+    map<pair<int,int>,int> m;
+    scanf("%d", &n);
+    graph g(n), edge_ids(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int v1, v2;
+        scanf("%d%d", &v1, &v2);
+        --v1;
+        --v2;
+        g[v1].push_back(v2);
+        g[v2].push_back(v1);
+        m[mp(v1,v2)]=i;
+        m[mp(v2,v1)]=i;
+        edge_ids[v1].push_back(i);
+        edge_ids[v2].push_back(i);
+    }
+
+    h.assign(n, -1);
+    dfs(0, g, edge_ids);
+    lca_prepare(n);
+    query_prepare(n);
+
+    while(1) {
+        int q;
+        cin>>q;
+        int a,b;
+        if (q==1) {
+            // request for painting edge x;
+            // if start = true, then the edge is painted, otherwise the painting
+            // is removed
+            int start;
+            cin>>start;
+            cin>>a>>b;
+            a--;
+            b--;
+            int x=m[mp(a,b)];
+            edge_used[x] = start;
+            sum_tree_update(tree1, 1, 0, (int)edges_list.size() - 1, first1[x],
+                            start ? 1 : -1);
+            sum_tree_update(tree2, 1, 0, (int)edges_list.size() - 1, first2[x],
+                            start ? 1 : -1);
+        } else {
+            cin>>a>>b;
+            a--;
+            b--;
+            // query the number of colored edges on the path between v1 and v2
+            int l = lca(a, b);
+            int result = query(l, a) + query(l, b);
+            cout<<result<<endl;
+            // result - the answer to the request
+        }
+    }
+}
+//int main(){
+//
+//    she_taught_me_to_code
+//
+//    // freopen("input.txt","r",stdin);
+//    // freopen("output.txt","w",stdout);
+//
+//
+//}
