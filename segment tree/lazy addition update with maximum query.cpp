886e5863d819efbb5b9af3efdbd8ee46fccae5a5
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2020-04-22 18:40:59 +0530

    Create lazy addition update with maximum query.cpp

---
+++ segment tree/lazy addition update with maximum query.cpp
@@ -0,0 +1,35 @@
+void push(ll node) {
+    tree[node*2] += lazy[node];
+    lazy[node*2] += lazy[node];
+    tree[node*2+1] += lazy[node];
+    lazy[node*2+1] += lazy[node];
+    lazy[node] = 0;
+}
+void update(ll node,ll left,ll right,ll l,ll r,ll add) {
+    if(r < left || right < l){
+        return;
+    }
+    if(l <= left && r >= right){
+        tree[node]+=add;
+        lazy[node]+=add;
+    }
+    else {
+        push(node);
+        ll mid = (left + right) / 2;
+        update(2*node,left,mid,l,r,add);
+        update(2*node+1,mid+1,right,l,r,add);
+        tree[node]=max(tree[node*2],tree[node*2+1]);
+    }
+}
+
+ll query(ll node,ll left,ll right,ll l,ll r){
+    if(l>r){
+        return -inf;
+    }
+    if(r < left || right < l){
+        return tree[node];
+    }
+    push(node);
+    ll mid=(left+right)/2;
+    return max(query(node*2,left,mid,l,r),query(node*2+1,mid+1,right,l,r));
+}
